020375c7d3186dc03d53bf043fefc16283f5f334
Alexandr4e <blacksbox@yandex.ru>
2018-04-19 14:48:44 +0500

    init new vue

---
+++ src/components/newComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <h1>THIS IS NEW COMPONENT</h1>
+    <div>{{ msg }}</div>
+    <div>{{ newText }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'componentName',
+    data () {
+      return {
+        msg: 'FUCK OFF ASSHOLE',
+        newText: 'PUTIN',
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    color: red;
+  }
+  .box {
+    background: brown;
+  }
+</style>
